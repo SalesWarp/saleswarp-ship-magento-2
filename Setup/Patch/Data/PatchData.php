49f95d5716a5be637b5ad78c1a0309d4c3609703
Sarvesh <sarveswaran.j@saleswarp.com>
2024-05-14 17:51:04 +0530

    * Added all files

---
+++ Setup/Patch/Data/PatchData.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Saleswarp\SaleswarpShip\Setup\Patch\Data;
+
+use Magento\Integration\Model\ConfigBasedIntegrationManager;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+/**
+ * Setup data patch class to change config path
+ */
+class PatchData implements DataPatchInterface
+{
+
+    /**
+     * @var ConfigBasedIntegrationManager
+     */
+     private $integrationManager;
+
+    /**
+     * @param ConfigBasedIntegrationManager $integrationManager
+     */
+    public function __construct(ConfigBasedIntegrationManager $integrationManager)
+    {
+        $this->integrationManager = $integrationManager;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function apply()
+    {
+        $this->integrationManager->processIntegrationConfig(['SaleswarpShip']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getVersion()
+    {
+        return '1.0.0';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
---
+++ Setup/PatchData.php
@@ -0,0 +1,35 @@
+<?php
+namespace Saleswarp\SaleswarpShip\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Integration\Model\ConfigBasedIntegrationManager;
+use Magento\Framework\Setup\InstallDataInterface;
+
+class PatchData implements InstallDataInterface
+{
+    /**
+     * @var ConfigBasedIntegrationManager
+     */
+    private $integrationManager;
+
+    /**
+     * @param ConfigBasedIntegrationManager $integrationManager
+     */
+    public function __construct(ConfigBasedIntegrationManager $integrationManager)
+    {
+        $this->integrationManager = $integrationManager;
+    }
+
+    /**
+     * Installs data for a module
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $this->integrationManager->processIntegrationConfig(['SaleswarpShip']);
+    }
+}
