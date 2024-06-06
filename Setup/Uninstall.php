83b2823e0c1a895fa89c8781efdb14999edccfc0
Sarvesh <sarveswaran.j@saleswarp.com>
2024-06-06 17:13:45 +0530

    * Added uninstall commands

---
+++ Setup/Uninstall.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Saleswarp\SaleswarpShip\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+
+class Uninstall implements UninstallInterface
+{
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $connection = $setup->getConnection();
+
+        $connection->delete($setup->getTable('patch_list'), ['patch_name = ?' => '%Saleswarp\SaleswarpShip%']);
+        $connection->delete($setup->getTable('integration'), ['name = ?' => '%SaleswarpShip%']);
+
+        $setup->endSetup();
+    }
+}
